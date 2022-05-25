05797488005c9c8a231cd31197f8b37e2ad746cf
PogodiPostoi <boombox577@gmail.com>
2022-05-25 17:09:01 +0300

    Lesson 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,69 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+const sneakers = [
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 12999,
+    image: "/img/sneakers/1.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 12999,
+    image: "/img/sneakers/2.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 8499,
+    image: "/img/sneakers/3.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 8999,
+    image: "/img/sneakers/4.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 15199,
+    image: "/img/sneakers/5.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 11299,
+    image: "/img/sneakers/6.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 10799,
+    image: "/img/sneakers/7.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 16499,
+    image: "/img/sneakers/8.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 13999,
+    image: "/img/sneakers/9.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 8499,
+    image: "/img/sneakers/10.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 8999,
+    image: "/img/sneakers/11.jpg"
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 11299,
+    image: "/img/sneakers/12.jpg"
+  }
+]
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -19,12 +82,15 @@ function App() {
         </div>
 
         <div className="d-flex flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            sneakers.map(el => (
+              <Card 
+                title = {el.name}
+                price = {el.price}
+                imageUrl = {el.image}
+              />
+            ))
+          }
         </div>
       </div>
     </div>
